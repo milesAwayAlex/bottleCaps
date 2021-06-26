ad68b68e730c6eb45044501a56054c945623a774
Alex M <milesAwayAlex@gmail.com>
2021-06-26 15:31:18 -0700

    Add comments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const recycle = inv => {
   const bought = Math.trunc(inv / 2);
+  // initialize the state
   const state = {
     total: bought,
     bottles: bought,
@@ -7,7 +8,9 @@ const recycle = inv => {
     earned: {b: 0, c: 0},
   };
   const recycle = st => {
+    // copy state to maintain purity
     st = JSON.parse(JSON.stringify(st));
+    // recycle two bottles and start over
     if (st.bottles > 1) {
       st.total++;
       st.bottles--;
@@ -15,6 +18,7 @@ const recycle = inv => {
       st.earned.b++;
       return recycle(st);
     }
+    // recycle four caps and start over
     if (st.caps > 3) {
       st.total++;
       st.bottles++;
@@ -22,12 +26,14 @@ const recycle = inv => {
       st.earned.c++;
       return recycle(st);
     }
+    // nothing to recycle - done
     return st;
   };
   return recycle(state);
 };
 export default recycle;
 
+// if a number is passed as an argument
 if (+process.argv[2]) {
   const st = recycle(+process.argv[2]);
   console.info(`
