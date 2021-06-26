6fd37b2b2fa56f81c2db20781cc3d7ce91cb8ba9
Alex M <milesAwayAlex@gmail.com>
2021-06-26 15:27:51 -0700

    Implement the function

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-export default () => {
-  const res = {total: 0, bottles: 0, caps: 0, earned: {b: 0, c: 0}};
-  return res;
+const recycle = inv => {
+  const bought = Math.trunc(inv / 2);
+  const state = {
+    total: bought,
+    bottles: bought,
+    caps: bought,
+    earned: {b: 0, c: 0},
+  };
+  const recycle = st => {
+    st = JSON.parse(JSON.stringify(st));
+    if (st.bottles > 1) {
+      st.total++;
+      st.bottles--;
+      st.caps++;
+      st.earned.b++;
+      return recycle(st);
+    }
+    if (st.caps > 3) {
+      st.total++;
+      st.bottles++;
+      st.caps -= 3;
+      st.earned.c++;
+      return recycle(st);
+    }
+    return st;
+  };
+  return recycle(state);
 };
+export default recycle;
+
+if (+process.argv[2]) {
+  const st = recycle(+process.argv[2]);
+  console.info(`
+TOTAL BOTTLES: ${st.total}
+REMAINING BOTTLES: ${st.bottles}
+REMAINING CAPS: ${st.caps}
+TOTAL EARNED:
+  BOTTLES: ${st.earned.b}
+  CAPS: ${st.earned.c}`);
+}
